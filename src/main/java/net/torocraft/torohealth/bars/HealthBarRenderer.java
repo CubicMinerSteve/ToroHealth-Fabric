c0f71c5ba40b6ccf8d97251887e75c3d5fc867ed
CubicMinerSteve <109577246+CubicMinerSteve@users.noreply.github.com>
2023-11-17 21:33:17 +0800

    Updated to Minecraft 1.20.2 and Gradle 8.4

--- src/main/java/net/torocraft/torohealth/bars/HealthBarRenderer.java
+++ src/main/java/net/torocraft/torohealth/bars/HealthBarRenderer.java
@@ -1,9 +1,15 @@
 package net.torocraft.torohealth.bars;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL11;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
@@ -14,194 +20,211 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.RotationAxis;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Vec3f;
 import net.torocraft.torohealth.ToroHealth;
 import net.torocraft.torohealth.config.Config;
 import net.torocraft.torohealth.config.Config.InWorld;
 import net.torocraft.torohealth.config.Config.Mode;
+import net.torocraft.torohealth.mixin.DrawContextMixin;
 import net.torocraft.torohealth.util.EntityUtil;
 import net.torocraft.torohealth.util.EntityUtil.Relation;
-import org.lwjgl.opengl.GL11;
 
 public class HealthBarRenderer {
 
-  private static final Identifier GUI_BARS_TEXTURES = new Identifier(ToroHealth.MODID + ":textures/gui/bars.png");
-  private static final int DARK_GRAY = 0x808080;
-  private static final float FULL_SIZE = 40;
+	private static final Identifier GUI_BARS_TEXTURES = new Identifier(ToroHealth.MODID + ":textures/gui/bars.png");
+	private static final int DARK_GRAY = 0x808080;
+	private static final float FULL_SIZE = 40;
 
-  private static InWorld getConfig() {
-    return ToroHealth.CONFIG.inWorld;
-  }
+	private static InWorld getConfig() {
+		return ToroHealth.CONFIG.inWorld;
+	}
 
-  private static final List<LivingEntity> renderedEntities = new ArrayList<>();
+	private static final List<LivingEntity> renderedEntities = new ArrayList<>();
 
-  public static void prepareRenderInWorld(LivingEntity entity) {
-    MinecraftClient client = MinecraftClient.getInstance();
+	public static void prepareRenderInWorld(LivingEntity entity) {
 
-    if (!EntityUtil.showHealthBar(entity, client)) {
-      return;
-    }
+		MinecraftClient client = MinecraftClient.getInstance();
 
-    if (entity.distanceTo(client.getCameraEntity()) > ToroHealth.CONFIG.inWorld.distance) {
-      return;
-    }
+		if (!EntityUtil.showHealthBar(entity, client)) {
+			return;
+		}
 
-    BarStates.getState(entity);
+		if (entity.distanceTo(client.getCameraEntity()) > ToroHealth.CONFIG.inWorld.distance) {
+			return;
+		}
 
-    if (Mode.WHEN_HOLDING_WEAPON.equals(getConfig().mode) && !ToroHealth.IS_HOLDING_WEAPON) {
-      return;
-    }
+		BarStates.getState(entity);
 
-    if (Mode.NONE.equals(getConfig().mode)) {
-      return;
-    }
+		if (Mode.WHEN_HOLDING_WEAPON.equals(getConfig().mode) && !ToroHealth.IS_HOLDING_WEAPON) {
+			return;
+		}
 
-    if (ToroHealth.CONFIG.inWorld.onlyWhenLookingAt && ToroHealth.HUD.getEntity() != entity) {
-      return;
-    }
+		if (Mode.NONE.equals(getConfig().mode)) {
+			return;
+		}
 
-    if (ToroHealth.CONFIG.inWorld.onlyWhenHurt && entity.getHealth() >= entity.getMaxHealth()) {
-      return;
-    }
+		if (ToroHealth.CONFIG.inWorld.onlyWhenLookingAt && ToroHealth.HUD.getEntity() != entity) {
+			return;
+		}
 
-    renderedEntities.add(entity);
+		if (ToroHealth.CONFIG.inWorld.onlyWhenHurt && entity.getHealth() >= entity.getMaxHealth()) {
+			return;
+		}
 
-  }
+		renderedEntities.add(entity);
+	}
 
-  public static void renderInWorld(MatrixStack matrix, Camera camera) {
+	public static void renderInWorld(MatrixStack matrix, Camera camera) {
 
-    MinecraftClient client = MinecraftClient.getInstance();
+		MinecraftClient client = MinecraftClient.getInstance();
 
-    if (camera == null) {
-      camera = client.getEntityRenderDispatcher().camera;
-    }
+		if (camera == null) {
+			camera = client.getEntityRenderDispatcher().camera;
+		}
 
-    if (camera == null) {
-      renderedEntities.clear();
-      return;
-    }
+		if (camera == null) {
+			renderedEntities.clear();
+			return;
+		}
 
-    if (renderedEntities.isEmpty()) {
-      return;
-    }
+		if (renderedEntities.isEmpty()) {
+			return;
+		}
 
-    RenderSystem.setShader(GameRenderer::getPositionColorShader);
-    RenderSystem.enableDepthTest();
-    RenderSystem.enableBlend();
-    RenderSystem.blendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE,
-        GL11.GL_ZERO);
+		RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+		RenderSystem.enableDepthTest();
+		RenderSystem.enableBlend();
+		RenderSystem.blendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
 
-    for (LivingEntity entity : renderedEntities) {
-      float scaleToGui = 0.025f;
-      boolean sneaking = entity.isInSneakingPose();
-      float height = entity.getHeight() + 0.6F - (sneaking ? 0.25F : 0.0F);
+		for (LivingEntity entity : renderedEntities) {
+			float scaleToGui = 0.025f;
+			boolean sneaking = entity.isInSneakingPose();
+			float height = entity.getHeight() + 0.6F - (sneaking ? 0.25F : 0.0F);
 
-      float tickDelta = client.getTickDelta();
-      double x = MathHelper.lerp((double) tickDelta, entity.prevX, entity.getX());
-      double y = MathHelper.lerp((double) tickDelta, entity.prevY, entity.getY());
-      double z = MathHelper.lerp((double) tickDelta, entity.prevZ, entity.getZ());
+			float tickDelta = client.getTickDelta();
+			double x = MathHelper.lerp((double) tickDelta, entity.prevX, entity.getX());
+			double y = MathHelper.lerp((double) tickDelta, entity.prevY, entity.getY());
+			double z = MathHelper.lerp((double) tickDelta, entity.prevZ, entity.getZ());
 
-      Vec3d camPos = camera.getPos();
-      double camX = camPos.x;
-      double camY = camPos.y;
-      double camZ = camPos.z;
+			Vec3d camPos = camera.getPos();
+			double camX = camPos.x;
+			double camY = camPos.y;
+			double camZ = camPos.z;
 
-      matrix.push();
-      matrix.translate(x - camX, (y + height) - camY, z - camZ);
-      matrix.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(-camera.getYaw()));
-      matrix.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(camera.getPitch()));
-      matrix.scale(-scaleToGui, -scaleToGui, scaleToGui);
+			matrix.push();
+			matrix.translate(x - camX, (y + height) - camY, z - camZ);
+			matrix.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-camera.getYaw()));
+			matrix.multiply(RotationAxis.POSITIVE_X.rotationDegrees(camera.getPitch()));
+			matrix.scale(-scaleToGui, -scaleToGui, scaleToGui);
 
-      render(matrix, entity, 0, 0, FULL_SIZE, true);
+			render(matrix, entity, 0, 0, FULL_SIZE, true);
 
-      matrix.pop();
-    }
+			matrix.pop();
+		}
 
-    RenderSystem.disableBlend();
+		RenderSystem.disableBlend();
 
-    renderedEntities.clear();
-  }
+		renderedEntities.clear();
+	}
 
-  public static void render(MatrixStack matrix, LivingEntity entity, double x, double y,
-      float width, boolean inWorld) {
+	public static void render(MatrixStack matrix, LivingEntity entity, double x, double y, float width, boolean inWorld) {
 
-    Relation relation = EntityUtil.determineRelation(entity);
+		// Get entity relation to players.
+		Relation relation = EntityUtil.determineRelation(entity);
+		// Set First Color and Second Color according to the relation.
+		int color = relation.equals(Relation.FRIEND) ? ToroHealth.CONFIG.bar.friendColor : ToroHealth.CONFIG.bar.foeColor;
+		int color2 = relation.equals(Relation.FRIEND) ? ToroHealth.CONFIG.bar.friendColorSecondary : ToroHealth.CONFIG.bar.foeColorSecondary;
 
-    int color = relation.equals(Relation.FRIEND) ? ToroHealth.CONFIG.bar.friendColor
-        : ToroHealth.CONFIG.bar.foeColor;
-    int color2 = relation.equals(Relation.FRIEND) ? ToroHealth.CONFIG.bar.friendColorSecondary
-        : ToroHealth.CONFIG.bar.foeColorSecondary;
+		BarState state = BarStates.getState(entity);
 
-    BarState state = BarStates.getState(entity);
+		float percent = Math.min(1, Math.min(state.health, entity.getMaxHealth()) / entity.getMaxHealth());
+		float percent2 = Math.min(state.previousHealthDisplay, entity.getMaxHealth()) / entity.getMaxHealth();
+		// Vertical offset.
+		int zOffset = 0;
+		// Get the Position Matrix of the current DrawContext.
+		Matrix4f m4f = matrix.peek().getPositionMatrix(); // .getModel();
+		drawBar(m4f, x, y, width, 1, DARK_GRAY, zOffset++, inWorld);
+		drawBar(m4f, x, y, width, percent2, color2, zOffset++, inWorld);
+		drawBar(m4f, x, y, width, percent, color, zOffset, inWorld);
 
-    float percent = Math.min(1, Math.min(state.health, entity.getMaxHealth()) / entity.getMaxHealth());
-    float percent2 = Math.min(state.previousHealthDisplay, entity.getMaxHealth()) / entity.getMaxHealth();
-    int zOffset = 0;
+		if (!inWorld) {
+			if (ToroHealth.CONFIG.bar.damageNumberType.equals(Config.NumberType.CUMULATIVE)) {
+				drawDamageNumber(state.lastDmgCumulative, x, y, width);
+			} else if (ToroHealth.CONFIG.bar.damageNumberType.equals(Config.NumberType.LAST)) {
+				drawDamageNumber(state.lastDmg, x, y, width);
+			}
+		}
+	}
 
-    Matrix4f m4f = matrix.peek().getPositionMatrix(); //  .getModel();
-    drawBar(m4f, x, y, width, 1, DARK_GRAY, zOffset++, inWorld);
-    drawBar(m4f, x, y, width, percent2, color2, zOffset++, inWorld);
-    drawBar(m4f, x, y, width, percent, color, zOffset, inWorld);
+	public static void drawDamageNumber(int dmg, double x, double y, float width) {
+		int i = Math.abs(Math.round(dmg));
+		if (i == 0) {
+			return;
+		}
+		String s = Integer.toString(i);
+		MinecraftClient minecraft = MinecraftClient.getInstance();
 
-    if (!inWorld) {
-      if (ToroHealth.CONFIG.bar.damageNumberType.equals(Config.NumberType.CUMULATIVE)) {
-        drawDamageNumber(matrix, state.lastDmgCumulative, x, y, width);
-      } else if (ToroHealth.CONFIG.bar.damageNumberType.equals(Config.NumberType.LAST)) {
-        drawDamageNumber(matrix, state.lastDmg, x, y, width);
-      }
-    }
-  }
+		int sw = minecraft.textRenderer.getWidth(s);
+		int color = dmg < 0 ? ToroHealth.CONFIG.particle.healColor : ToroHealth.CONFIG.particle.damageColor;
 
-  public static void drawDamageNumber(MatrixStack matrix, int dmg, double x, double y,
-      float width) {
-    int i = Math.abs(Math.round(dmg));
-    if (i == 0) {
-      return;
-    }
-    String s = Integer.toString(i);
-    MinecraftClient minecraft = MinecraftClient.getInstance();
-    int sw = minecraft.textRenderer.getWidth(s);
-    int color = dmg < 0 ? ToroHealth.CONFIG.particle.healColor : ToroHealth.CONFIG.particle.damageColor;
-    minecraft.textRenderer.draw(matrix, s, (int) (x + (width / 2) - sw), (int) y + 5, color);
-  }
+		DrawContext drawContext = new DrawContext(minecraft, minecraft.getBufferBuilders().getEffectVertexConsumers());
+		drawContext.drawTextWithShadow(minecraft.textRenderer, s, (int) (x + (width / 2) - sw) + 50, (int) y + 10, color);	
+	}
 
-  private static void drawBar(Matrix4f matrix4f, double x, double y, float width, float percent,
-      int color, int zOffset, boolean inWorld) {
-    float c = 0.00390625F;
-    int u = 0;
-    int v = 6 * 5 * 2 + 5;
-    int uw = MathHelper.ceil(92 * percent);
-    int vh = 5;
+	public static void drawDamageNumberInWorld(MatrixStack matrix, int dmg) {
+    	int i = Math.abs(Math.round(dmg));
+		if (i == 0) {
+			return;
+		}
+		String s = Integer.toString(i);
+		MinecraftClient minecraft = MinecraftClient.getInstance();
 
-    double size = percent * width;
-    double h = inWorld ? 4 : 6;
+		int color = dmg < 0 ? ToroHealth.CONFIG.particle.healColor : ToroHealth.CONFIG.particle.damageColor;
 
-    float r = (color >> 16 & 255) / 255.0F;
-    float g = (color >> 8 & 255) / 255.0F;
-    float b = (color & 255) / 255.0F;
+		DrawContext drawContext = new DrawContext(minecraft, minecraft.getBufferBuilders().getEffectVertexConsumers());
+ 
+		((DrawContextMixin) drawContext).setMatrices(matrix);
+		drawContext.drawText(minecraft.textRenderer, s, 0, 0, color, false);
+  	}
 
-    RenderSystem.setShaderColor(r, g, b, 1);
-    RenderSystem.setShader(GameRenderer::getPositionTexShader);
-    RenderSystem.setShaderTexture(0, GUI_BARS_TEXTURES);
-    RenderSystem.enableBlend();
+	private static void drawBar(Matrix4f matrix4f, double x, double y, float width, float percent, int color, int zOffset, boolean inWorld) {
+		float c = 0.00390625F;
+		int u = 0; // 横向材质显示起始点
+		int v = 6 * 5 * 2 + 5;
+		int uw = MathHelper.ceil(92 * percent); // 横向材质显示范围像素量，对应血量百分比。 92 为材质有效宽度
+		int vh = 5;
 
-    float half = width / 2;
+		double size = percent * width;
+		double h = inWorld ? 4 : 6;
 
-    float zOffsetAmount = inWorld ? -0.1F : 0.1F;
+		float r = (color >> 16 & 255) / 255.0F;
+		float g = (color >> 8 & 255) / 255.0F;
+		float b = (color & 255) / 255.0F;
 
-    Tessellator tessellator = Tessellator.getInstance();
-    BufferBuilder buffer = tessellator.getBuffer();
-    buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-    buffer.vertex(matrix4f, (float) (-half + x), (float) y, zOffset * zOffsetAmount)
-        .texture(u * c, v * c).next();
-    buffer.vertex(matrix4f, (float) (-half + x), (float) (h + y), zOffset * zOffsetAmount)
-        .texture(u * c, (v + vh) * c).next();
-    buffer.vertex(matrix4f, (float) (-half + size + x), (float) (h + y), zOffset * zOffsetAmount)
-        .texture((u + uw) * c, (v + vh) * c).next();
-    buffer.vertex(matrix4f, (float) (-half + size + x), (float) y, zOffset * zOffsetAmount)
-        .texture(((u + uw) * c), v * c).next();
-    tessellator.draw();
-  }
+		RenderSystem.setShaderColor(r, g, b, 1);
+		RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+		RenderSystem.setShaderTexture(0, GUI_BARS_TEXTURES);
+		RenderSystem.enableBlend();
+
+		float half = width / 2;
+
+		float zOffsetAmount = inWorld ? -0.1F : 0.1F;
+
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder buffer = tessellator.getBuffer();
+		buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+		buffer.vertex(matrix4f, (float) (-half + x), (float) y, zOffset * zOffsetAmount)
+				.texture(u * c, v * c).next();
+		buffer.vertex(matrix4f, (float) (-half + x), (float) (h + y), zOffset * zOffsetAmount)
+				.texture(u * c, (v + vh) * c).next();
+		buffer.vertex(matrix4f, (float) (-half + size + x), (float) (h + y), zOffset * zOffsetAmount)
+				.texture((u + uw) * c, (v + vh) * c).next();
+		buffer.vertex(matrix4f, (float) (-half + size + x), (float) y, zOffset * zOffsetAmount)
+				.texture(((u + uw) * c), v * c).next();
+		tessellator.draw();
+
+		RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+	}
+
 }
--- src/main/java/net/torocraft/torohealth/config/loader/FileWatcher.java
+++ src/main/java/net/torocraft/torohealth/config/loader/FileWatcher.java
@@ -2,6 +2,7 @@ package net.torocraft.torohealth.config.loader;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.ClosedWatchServiceException;
@@ -12,59 +13,59 @@ import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
 public class FileWatcher implements Runnable {
-  private final File file;
-  private final Path filename;
-  private final Path parent;
-  private final Listener listener;
 
-  @FunctionalInterface
-  public static interface Listener {
-    void onUpdate();
-  }
+	private final File file;
+	private final Path filename;
+	private final Path parent;
+	private final Listener listener;
 
-  public static FileWatcher watch(File file, Listener listener) {
-    FileWatcher watcher = new FileWatcher(file, listener);
-    Thread thread = new Thread(watcher);
-    thread.setDaemon(true);
-    thread.start();
-    return watcher;
-  }
+	@FunctionalInterface
+	public static interface Listener {
+		void onUpdate();
+	}
 
-  private FileWatcher(File file, Listener listener) {
-    this.file = file;
-    this.listener = listener;
-    this.filename = file.toPath().getFileName();
-    this.parent = file.toPath().getParent();
-  }
+	public static FileWatcher watch(File file, Listener listener) {
+		FileWatcher watcher = new FileWatcher(file, listener);
+		Thread thread = new Thread(watcher);
+		thread.setDaemon(true);
+		thread.start();
+		return watcher;
+	}
 
-  @Override
-  public void run() {
-    try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
-      this.parent.register(watchService, ENTRY_MODIFY, ENTRY_CREATE);
-      boolean poll = true;
-      while (poll) {
-        poll = pollEvents(watchService);
-      }
-    } catch (IOException | InterruptedException | ClosedWatchServiceException e) {
-      Thread.currentThread().interrupt();
-    }
-  }
+	private FileWatcher(File file, Listener listener) {
+		this.file = file;
+		this.listener = listener;
+		this.filename = file.toPath().getFileName();
+		this.parent = file.toPath().getParent();
+	}
 
-  protected boolean pollEvents(WatchService watchService) throws InterruptedException {
-    WatchKey key = watchService.take();
-    for (WatchEvent<?> event : key.pollEvents()) {
-      Path changedFilename = ((Path) event.context()).getFileName();
-      if (changedFilename.equals(filename)) {
-        try {
-          listener.onUpdate();
-        } catch (Exception e) {
-          new Exception("Error during file watch of " + file.getAbsolutePath(), e)
-              .printStackTrace();
-        }
-      }
-    }
-    return key.reset();
-  }
+	@Override
+	public void run() {
+		try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
+			this.parent.register(watchService, ENTRY_MODIFY, ENTRY_CREATE);
+			boolean poll = true;
+			while (poll) {
+				poll = pollEvents(watchService);
+			}
+		} catch (IOException | InterruptedException | ClosedWatchServiceException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	protected boolean pollEvents(WatchService watchService) throws InterruptedException {
+		WatchKey key = watchService.take();
+		for (WatchEvent<?> event : key.pollEvents()) {
+			Path changedFilename = ((Path) event.context()).getFileName();
+			if (changedFilename.equals(filename)) {
+				try {
+					listener.onUpdate();
+				} catch (Exception e) {
+					new Exception("Error during file watch of " + file.getAbsolutePath(), e)
+							.printStackTrace();
+				}
+			}
+		}
+		return key.reset();
+	}
 
 }
-
